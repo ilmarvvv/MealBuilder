8c05a7311e45b83097b86ab548d3a569afed3a92
Illia Sheviakov <ilmarvvv@gmail.com>
2026-04-28 21:06:12 +0200

    feat(data): configure EF Core SQLite context

--- MealBuilder.Web/Program.cs
+++ MealBuilder.Web/Program.cs
@@ -1,7 +1,13 @@
+using MealBuilder.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
