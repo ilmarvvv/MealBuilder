4e79ab9b04f5d267416030babd214bc57bc647ba
Illia Sheviakov <ilmarvvv@gmail.com>
2026-04-28 21:31:39 +0200

    feat(database): add initial Ingredient migration

---
+++ MealBuilder.Web/Migrations/20260428192345_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MealBuilder.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    CaloriesPer100g = table.Column<decimal>(type: "TEXT", nullable: false),
+                    ProteinPer100g = table.Column<decimal>(type: "TEXT", nullable: false),
+                    FiberPer100g = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+        }
+    }
+}
