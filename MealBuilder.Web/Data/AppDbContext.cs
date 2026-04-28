3bf323f8c6f25615b8a4e1f332b4a9cdccfd73d0
Illia Sheviakov <ilmarvvv@gmail.com>
2026-04-28 20:34:11 +0200

    feat(data): add AppDbContext

---
+++ MealBuilder.Web/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MealBuilder.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealBuilder.Web.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Ingredient> Ingredients { get; set; }
+    }
+}
