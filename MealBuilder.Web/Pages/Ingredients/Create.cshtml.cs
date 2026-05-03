c810c477c29aa7e41eb08c4dd838414d14604fb9
Illia Sheviakov <ilmarvvv@gmail.com>
2026-05-03 18:03:20 +0200

    feat(ingredients): add create ingredient page

---
+++ MealBuilder.Web/Pages/Ingredients/Create.cshtml.cs
@@ -0,0 +1,37 @@
+using MealBuilder.Web.Data;
+using MealBuilder.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MealBuilder.Web.Pages.Ingredients
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext _context;
+
+        public CreateModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Ingredient Ingredient { get; set; } = new();
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Ingredients.Add(Ingredient);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
