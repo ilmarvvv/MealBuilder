4caf1b0717997e1262cce056109a6a273d7e7666
Illia Sheviakov <ilmarvvv@gmail.com>
2026-05-11 20:47:24 +0200

    feat(models): add RecipeIngredient model

---
+++ MealBuilder.Web/Models/RecipeIngredient.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MealBuilder.Web.Models
+{
+    public class RecipeIngredient
+    {
+        public int Id { get; set; }
+
+        public int RecipeId { get; set; }
+
+        public Recipe Recipe { get; set; } = null!;
+
+        public int IngredientId { get; set; }
+
+        public Ingredient Ingredient { get; set; } = null!;
+
+        [Range(0.01, 100000)]
+        public decimal Grams { get; set; }
+    }
+}
