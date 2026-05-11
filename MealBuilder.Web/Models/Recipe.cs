af9196ca2b2501c03ae2d1bc809133cc0a921856
Illia Sheviakov <ilmarvvv@gmail.com>
2026-05-11 20:37:23 +0200

    feat(models): add Recipe model

---
+++ MealBuilder.Web/Models/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MealBuilder.Web.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(1000)]
+        public string? Description { get; set; }
+
+        [Range(1, 100)]
+        public int Servings { get; set; } = 1;
+    }
+}
