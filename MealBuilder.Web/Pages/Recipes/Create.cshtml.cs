81d782367adef961e8851a015e7eee31afd098e4
Illia Sheviakov <ilmarvvv@gmail.com>
2026-05-12 18:38:36 +0200

    feat(recipes): add create recipe page

---
+++ MealBuilder.Web/Pages/Recipes/Create.cshtml.cs
@@ -0,0 +1,37 @@
+using MealBuilder.Web.Data;
+using MealBuilder.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MealBuilder.Web.Pages.Recipes
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext _context;
+
+        public CreateModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Recipe Recipe { get; set; } = new();
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Recipes.Add(Recipe);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
