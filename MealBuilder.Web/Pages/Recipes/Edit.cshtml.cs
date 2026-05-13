6f98bc20c6f3ab67b48d13deaf8a09b23c0fce8c
Illia Sheviakov <ilmarvvv@gmail.com>
2026-05-13 20:53:54 +0200

    feat(recipes): add edit recipe page

---
+++ MealBuilder.Web/Pages/Recipes/Edit.cshtml.cs
@@ -0,0 +1,47 @@
+using MealBuilder.Web.Data;
+using MealBuilder.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MealBuilder.Web.Pages.Recipes
+{
+    public class EditModel : PageModel
+    {
+        private readonly AppDbContext _context;
+
+        public EditModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Recipe Recipe { get; set; } = new();
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Recipe? recipe = await _context.Recipes.FindAsync(id);
+
+            if (recipe is null)
+            {
+                return NotFound();
+            }
+
+            Recipe = recipe;
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Recipes.Update(Recipe);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
