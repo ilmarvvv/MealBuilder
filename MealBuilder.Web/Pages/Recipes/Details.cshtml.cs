31328d90343231e32ee198b64cfa513f1ff31027
Illia Sheviakov <ilmarvvv@gmail.com>
2026-05-14 09:49:51 +0200

    feat(recipes): show ingredients on details page

--- MealBuilder.Web/Pages/Recipes/Details.cshtml.cs
+++ MealBuilder.Web/Pages/Recipes/Details.cshtml.cs
@@ -2,12 +2,12 @@ using MealBuilder.Web.Data;
 using MealBuilder.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace MealBuilder.Web.Pages.Recipes
 {
     public class DetailsModel : PageModel
     {
-
         private readonly AppDbContext _context;
 
         public DetailsModel(AppDbContext context)
@@ -19,7 +19,10 @@ namespace MealBuilder.Web.Pages.Recipes
 
         public async Task<IActionResult> OnGetAsync(int id)
         {
-            Recipe? recipe = await _context.Recipes.FindAsync(id);
+            Recipe? recipe = await _context.Recipes
+                .Include(recipe => recipe.RecipeIngredients)
+                .ThenInclude(recipeIngredient => recipeIngredient.Ingredient)
+                .FirstOrDefaultAsync(recipe => recipe.Id == id);
 
             if (recipe is null)
             {
