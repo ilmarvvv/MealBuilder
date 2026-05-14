9b27a6327c7cc86a5aeaed0d3ef89c4505a418df
Illia Sheviakov <ilmarvvv@gmail.com>
2026-05-14 10:09:34 +0200

    feat(recipes): add ingredient to recipe

---
+++ MealBuilder.Web/Pages/Recipes/AddIngredient.cshtml.cs
@@ -0,0 +1,76 @@
+using MealBuilder.Web.Data;
+using MealBuilder.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealBuilder.Web.Pages.Recipes
+{
+    public class AddIngredientModel : PageModel
+    {
+        private readonly AppDbContext _context;
+
+        public AddIngredientModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Recipe Recipe { get; set; } = new();
+
+        public SelectList Ingredients { get; set; } = null!;
+
+        [BindProperty]
+        public RecipeIngredient RecipeIngredient { get; set; } = new();
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Recipe? recipe = await _context.Recipes.FindAsync(id);
+
+            if (recipe is null)
+            {
+                return NotFound();
+            }
+
+            Recipe = recipe;
+            RecipeIngredient.RecipeId = recipe.Id;
+
+            await LoadIngredientsAsync();
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            ModelState.Remove("RecipeIngredient.Recipe");
+            ModelState.Remove("RecipeIngredient.Ingredient");
+
+            if (!ModelState.IsValid)
+            {
+                Recipe? recipe = await _context.Recipes.FindAsync(RecipeIngredient.RecipeId);
+
+                if (recipe is not null)
+                {
+                    Recipe = recipe;
+                }
+
+                await LoadIngredientsAsync();
+                return Page();
+            }
+
+            _context.RecipeIngredients.Add(RecipeIngredient);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Details", new { id = RecipeIngredient.RecipeId });
+        }
+
+        private async Task LoadIngredientsAsync()
+        {
+            List<Ingredient> ingredients = await _context.Ingredients
+                .OrderBy(ingredient => ingredient.Name)
+                .ToListAsync();
+
+            Ingredients = new SelectList(ingredients, "Id", "Name");
+        }
+    }
+}
