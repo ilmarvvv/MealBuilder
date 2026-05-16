c3dea1f9a1c821fcb78e5cdf109a7c7297103d5b
Illia Sheviakov <ilmarvvv@gmail.com>
2026-05-16 14:51:04 +0200

    feat(recipes): calculate totals from ingredients

---
+++ MealBuilder.Web/Services/RecipeCalculationService.cs
@@ -0,0 +1,23 @@
+﻿using MealBuilder.Web.Models;
+
+namespace MealBuilder.Web.Services
+{
+    public class RecipeCalculationService
+    {
+        public RecipeNutritionTotals Calculate(Recipe recipe)
+        {
+            RecipeNutritionTotals totals = new();
+
+            foreach (RecipeIngredient recipeIngredient in recipe.RecipeIngredients)
+            {
+                totals.Calories += recipeIngredient.Ingredient.CaloriesPer100g * recipeIngredient.Grams / 100;
+                totals.Protein += recipeIngredient.Ingredient.ProteinPer100g * recipeIngredient.Grams / 100;
+                totals.Fiber += recipeIngredient.Ingredient.FiberPer100g * recipeIngredient.Grams / 100;
+                totals.Sugar += recipeIngredient.Ingredient.SugarPer100g * recipeIngredient.Grams / 100;
+                totals.Salt += recipeIngredient.Ingredient.SaltPer100g * recipeIngredient.Grams / 100;
+            }
+
+            return totals;
+        }
+    }
+}
