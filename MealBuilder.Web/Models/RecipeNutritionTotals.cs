66117899bb7b40f29fa8003cbc872aaac1001f69
Illia Sheviakov <ilmarvvv@gmail.com>
2026-05-16 14:43:26 +0200

    feat(recipes): add nutrition totals model

---
+++ MealBuilder.Web/Models/RecipeNutritionTotals.cs
@@ -0,0 +1,15 @@
+﻿namespace MealBuilder.Web.Models
+{
+    public class RecipeNutritionTotals
+    {
+        public decimal Calories { get; set; }
+
+        public decimal Protein { get; set; }
+
+        public decimal Fiber { get; set; }
+
+        public decimal Sugar { get; set; }
+
+        public decimal Salt { get; set; }
+    }
+}
