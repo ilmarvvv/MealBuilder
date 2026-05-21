31bbc40cf2bec29a1a55a693c626ceb456ad9d90
Illia Sheviakov <ilmarvvv@gmail.com>
2026-05-21 19:32:53 +0200

    feat(batches): add create prepared batch page

---
+++ MealBuilder.Web/Pages/PreparedRecipeBatches/Create.cshtml.cs
@@ -0,0 +1,54 @@
+using MealBuilder.Web.Data;
+using MealBuilder.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealBuilder.Web.Pages.PreparedRecipeBatches
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext _context;
+
+        public CreateModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public PreparedRecipeBatch PreparedRecipeBatch { get; set; } = new();
+
+        public SelectList RecipeSelectList { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            PreparedRecipeBatch.CookedDate = DateOnly.FromDateTime(DateTime.Today);
+
+            await LoadRecipeSelectListAsync();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                await LoadRecipeSelectListAsync();
+                return Page();
+            }
+
+            _context.PreparedRecipeBatches.Add(PreparedRecipeBatch);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+
+        private async Task LoadRecipeSelectListAsync()
+        {
+            List<Recipe> recipes = await _context.Recipes
+                .OrderBy(recipe => recipe.Name)
+                .ToListAsync();
+
+            RecipeSelectList = new SelectList(recipes, "Id", "Name");
+        }
+    }
+}
