c715c80c687e6422782945c38a33f8948a41ec43
Illia Sheviakov <ilmarvvv@gmail.com>
2026-05-22 13:19:16 +0200

    feat(menus): add edit menu page

---
+++ MealBuilder.Web/Pages/Menus/Edit.cshtml.cs
@@ -0,0 +1,48 @@
+using MealBuilder.Web.Data;
+using MealBuilder.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealBuilder.Web.Pages.Menus
+{
+    public class EditModel : PageModel
+    {
+        private readonly AppDbContext _context;
+
+        public EditModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Menu Menu { get; set; } = new();
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Menu? menu = await _context.Menus.FindAsync(id);
+
+            if (menu is null)
+            {
+                return NotFound();
+            }
+
+            Menu = menu;
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Menu).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Details", new { id = Menu.Id });
+        }
+    }
+}
