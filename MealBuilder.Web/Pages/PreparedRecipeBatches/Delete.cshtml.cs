0917e684af67261a13fd3ba5c2758d46d238d2a9
Illia Sheviakov <ilmarvvv@gmail.com>
2026-05-22 14:17:19 +0200

    feat(batches): add prepared batch delete page

---
+++ MealBuilder.Web/Pages/PreparedRecipeBatches/Delete.cshtml.cs
@@ -0,0 +1,79 @@
+using MealBuilder.Web.Data;
+using MealBuilder.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealBuilder.Web.Pages.PreparedRecipeBatches
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly AppDbContext _context;
+
+        public DeleteModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public PreparedRecipeBatchSummary PreparedRecipeBatchSummary { get; set; } = new();
+
+        public bool CanDelete => PreparedRecipeBatchSummary.UsedServings == 0;
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            PreparedRecipeBatch? preparedRecipeBatch = await _context.PreparedRecipeBatches
+                .Include(preparedRecipeBatch => preparedRecipeBatch.Recipe)
+                .Include(preparedRecipeBatch => preparedRecipeBatch.MenuItems)
+                .FirstOrDefaultAsync(preparedRecipeBatch => preparedRecipeBatch.Id == id);
+
+            if (preparedRecipeBatch is null)
+            {
+                return NotFound();
+            }
+
+            SetPreparedRecipeBatchSummary(preparedRecipeBatch);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            PreparedRecipeBatch? preparedRecipeBatch = await _context.PreparedRecipeBatches
+                .Include(preparedRecipeBatch => preparedRecipeBatch.MenuItems)
+                .FirstOrDefaultAsync(preparedRecipeBatch => preparedRecipeBatch.Id == id);
+
+            if (preparedRecipeBatch is null)
+            {
+                return NotFound();
+            }
+
+            decimal usedServings = CalculateUsedServings(preparedRecipeBatch);
+
+            if (usedServings > 0)
+            {
+                return RedirectToPage("./Details", new { id });
+            }
+
+            _context.PreparedRecipeBatches.Remove(preparedRecipeBatch);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+
+        private void SetPreparedRecipeBatchSummary(PreparedRecipeBatch preparedRecipeBatch)
+        {
+            PreparedRecipeBatchSummary = new PreparedRecipeBatchSummary
+            {
+                PreparedRecipeBatch = preparedRecipeBatch,
+                UsedServings = CalculateUsedServings(preparedRecipeBatch)
+            };
+        }
+
+        private static decimal CalculateUsedServings(PreparedRecipeBatch preparedRecipeBatch)
+        {
+            return preparedRecipeBatch.MenuItems
+                .Where(menuItem => menuItem.ServingsCount is not null)
+                .Sum(menuItem => menuItem.ServingsCount!.Value);
+        }
+    }
+}
