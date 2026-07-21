f31a463b4d1d2d62c9772b868759a9430b93b48f
Illia Sheviakov <ilmarvvv@gmail.com>
2026-07-21 17:26:14 +0200

    feat(infrastructure): configure identity persistence

--- MealBuilder.Api/Program.cs
+++ MealBuilder.Api/Program.cs
@@ -1,8 +1,28 @@
+using MealBuilder.Infrastructure.Data;
+using MealBuilder.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("AppDbContext")
+    ?? throw new InvalidOperationException(
+        "Connection string 'AppDbContext' was not found.");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(connectionString));
+
+builder.Services
+    .AddIdentityCore<ApplicationUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
---
+++ MealBuilder.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MealBuilder.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealBuilder.Infrastructure.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
